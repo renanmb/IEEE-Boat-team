425088f66c15518fbba71719b924b2aaf838dc25
Renan <renanm.b3@gmail.com>
2018-04-19 17:32:30 -0500

    Add files via upload

---
+++ ROS/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export LD_LIBRARY_PATH="/home/boat-team/catkin_ws/install/lib/x86_64-linux-gnu:/home/boat-team/catkin_ws/devel/lib/x86_64-linux-gnu:/opt/ros/jade/lib/x86_64-linux-gnu:/home/boat-team/catkin_ws/install/lib:/home/boat-team/catkin_ws/devel/lib:/opt/ros/jade/lib"
+export PKG_CONFIG_PATH="/home/boat-team/catkin_ws/install/lib/x86_64-linux-gnu/pkgconfig:/home/boat-team/catkin_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/jade/lib/x86_64-linux-gnu/pkgconfig:/home/boat-team/catkin_ws/install/lib/pkgconfig:/home/boat-team/catkin_ws/devel/lib/pkgconfig:/opt/ros/jade/lib/pkgconfig"
+export PWD="/home/boat-team/catkin_ws/build"
