94bfb908c7393e6908b4ad9a91149be309bc7855
Renan <renanm.b3@gmail.com>
2018-04-19 17:40:21 -0500

    Add files via upload

---
+++ ROS/catkin_ws/build/roboboat/CMakeFiles/_roboboat_generate_messages_check_deps_BoatHealth.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_roboboat_generate_messages_check_deps_BoatHealth"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang)
+  include(CMakeFiles/_roboboat_generate_messages_check_deps_BoatHealth.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
